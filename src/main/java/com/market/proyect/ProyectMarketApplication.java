2e15f810d70ff1460c218f24d47b06ccb005052a
lucamc <lucas.custodio9416@gmail.com>
2023-01-07 19:00:38 -0300

    Add api-market proyect with spring boot

---
+++ src/main/java/com/market/proyect/ProyectMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/proyect/ProyectMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
